166bad705e7b12f92d7efcf2eb1609eed2289629
Scott Barron <scott@barron.io>
2021-10-25 14:40:09 -0400

    copy to clipboard function

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,5 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+new ClipboardJS('.btn')
